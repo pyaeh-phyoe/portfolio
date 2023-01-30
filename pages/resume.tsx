2a2340357e5b93740e9054b5dc0682ee8a63bbe4
pyaeh-phyoe-ko-ko <pyaehphyoekoko15@gmail.com>
2023-01-30 12:57:44 +0630

    update resuem

---
+++ pages/resume.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+const Resume = () => {
+        return (<main className='min-h-screen'>
+        <iframe className='w-full h-screen' src="/resume.pdf"></iframe>
+        </main>)
+}
+ 
+export default Resume;
